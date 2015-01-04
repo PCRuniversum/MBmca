e8ab09d3f0e6d30320e3215ba8641a121c5292f2
michbur <michalburdukiewicz@gmail.com>
2015-01-04 21:57:25 +0100

    warnings fixed

--- R/datasets.R
+++ R/datasets.R
@@ -17,26 +17,26 @@
 #' Celsius, 1 degree Celsius per step) followed by melting curves of VIM,
 #' MLC-2v, SERCA2, Poly(dA)20, aCS and HPRT1 with bimodal melting pattern. The
 #' dyes and quencher used were Atto 647N and BHQ2.  \describe{
-#' \item{list("T")}{a numeric vector, Temperature in degree Celsius.}
+#' \item{T}{a numeric vector, Temperature in degree Celsius.}
 #' 
-#' \item{list("VIM.no.Q")}{a numeric vector, VIM without quencher and without
+#' \item{VIM.no.Q}{a numeric vector, VIM without quencher and without
 #' Poly(dT)20 region.}
 #' 
-#' \item{list("MLC2v.w.Q.w.dT20")}{a numeric vector, MLC-2v with
+#' \item{MLC2v.w.Q.w.dT20}{a numeric vector, MLC-2v with
 #' quencher-labeled detection probe and fluorescent Poly(dA)20 detection
 #' probe.}
 #' 
-#' \item{list("SERCA2.no.Q.w.dT20")}{a numeric vector, SERCA2 without
+#' \item{SERCA2.no.Q.w.dT20}{a numeric vector, SERCA2 without
 #' quencher-labeled detection probe and Poly(dA)20 detection probe.}
 #' 
-#' \item{list("PolydA20.w.Q")}{a numeric vector, Poly(dT)20 with fluorescent
+#' \item{PolydA20.w.Q}{a numeric vector, Poly(dT)20 with fluorescent
 #' Poly(dA)20 detection probe (quencher labeled).}
 #' 
-#' \item{list("aCS.w.Q.w.dT20")}{a numeric vector, artificial Control Sequence
+#' \item{aCS.w.Q.w.dT20}{a numeric vector, artificial Control Sequence
 #' without quencher-labeled detection probe and fluorescent Poly(dA)20
 #' detection probe.}
 #' 
-#' \item{list("HPRT1.no.Q.w.dT20")}{a numeric vector, HPRT1 without
+#' \item{HPRT1.no.Q.w.dT20}{a numeric vector, HPRT1 without
 #' quencher-labeled detection probe and fluorescent Poly(dA)20 detection
 #' probe.} }
 #' @seealso \code{\link{MFIerror}}, \code{\link{mcaSmoother}},
@@ -88,19 +88,19 @@ NULL
 #' detection probe pairs for HRPT1 and MLC-2v. First column contains the
 #' temperature (in degree Celsius, 0.5 degree Celsius per step) followed by
 #' melting curves of HRPT1 on 12 microbead populations and melting curves of
-#' MLC-2v on 12 microbead populations.  \describe{ \item{list("T")}{a numeric
+#' MLC-2v on 12 microbead populations.  \describe{ \item{T}{a numeric
 #' vector, Temperature in degree Celsius.}
 #' 
-#' \item{list("MLC2v")}{a numeric vector, MLC-2v with quencher-labeled
+#' \item{MLC2v}{a numeric vector, MLC-2v with quencher-labeled
 #' detection probe}
 #' 
-#' \item{list("SERCA2")}{a numeric vector, SERCA2 without quencher-labeled
+#' \item{SERCA2}{a numeric vector, SERCA2 without quencher-labeled
 #' detection probe}
 #' 
-#' \item{list("VIM.w.Mutation")}{a numeric vector, mutated VIM with
+#' \item{VIM.w.Mutation}{a numeric vector, mutated VIM with
 #' quencher-labeled detection probe}
 #' 
-#' \item{list("VIM.wo.Mutation")}{a numeric vector, native VIM with
+#' \item{VIM.wo.Mutation}{a numeric vector, native VIM with
 #' quencher-labeled detection probe} }
 #' @seealso \code{\link{MFIerror}}, \code{\link{mcaSmoother}},
 #' \code{\link{diffQ}}, \code{\link{diffQ2}}, \code{\link{DMP}},
@@ -151,35 +151,35 @@ NULL
 #' detection probe pairs for HRPT1 and MLC-2v. First column contains the
 #' temperature (in degree Celsius, 1 degree Celsius per step) followed by
 #' melting curves of HRPT1 on twelve microbead populations and melting curves
-#' of MLC-2v on twelve microbead populations.  \describe{ \item{list("T")}{a
+#' of MLC-2v on twelve microbead populations.  \describe{ \item{T}{a
 #' numeric vector for the temperature in degree Celsius}
-#' \item{list("HPRT1.1")}{a numeric vector, as HPRT1.1 of detection/capture
-#' probe HPRT1/HPRT1-cap on microbead population 1} \item{list("HPRT1.2")}{a
+#' \item{HPRT1.1}{a numeric vector, as HPRT1.1 of detection/capture
+#' probe HPRT1/HPRT1-cap on microbead population 1} \item{HPRT1.2}{a
 #' numeric vector, as HPRT1.2 on microbead population 2}
-#' \item{list("HPRT1.3")}{a numeric vector, as HPRT1.3 on microbead population
-#' 3} \item{list("HPRT1.4")}{a numeric vector, as HPRT1.4 on microbead
-#' population 4} \item{list("HPRT1.5")}{a numeric vector, as HPRT1.5 on
-#' microbead population 5} \item{list("HPRT1.6")}{a numeric vector, as HPRT1.6
-#' on microbead population 6} \item{list("HPRT1.7")}{a numeric vector, as
-#' HPRT1.7 on microbead population 7} \item{list("HPRT1.8")}{a numeric vector,
-#' as HPRT1.8 on microbead population 8} \item{list("HPRT1.9")}{a numeric
-#' vector, as HPRT1.9 on microbead population 9} \item{list("HPRT1.10")}{a
+#' \item{HPRT1.3}{a numeric vector, as HPRT1.3 on microbead population
+#' 3} \item{HPRT1.4}{a numeric vector, as HPRT1.4 on microbead
+#' population 4} \item{HPRT1.5}{a numeric vector, as HPRT1.5 on
+#' microbead population 5} \item{HPRT1.6}{a numeric vector, as HPRT1.6
+#' on microbead population 6} \item{HPRT1.7}{a numeric vector, as
+#' HPRT1.7 on microbead population 7} \item{HPRT1.8}{a numeric vector,
+#' as HPRT1.8 on microbead population 8} \item{HPRT1.9}{a numeric
+#' vector, as HPRT1.9 on microbead population 9} \item{HPRT1.10}{a
 #' numeric vector, as HPRT1.10 on microbead population 10}
-#' \item{list("HPRT1.11")}{a numeric vector, as HPRT1.11 on microbead
-#' population 11} \item{list("HPRT1.12")}{a numeric vector, as HPRT1.12 on
-#' microbead population 12} \item{list("MLC2v1")}{a numeric vector, as MLC2v1
+#' \item{HPRT1.11}{a numeric vector, as HPRT1.11 on microbead
+#' population 11} \item{HPRT1.12}{a numeric vector, as HPRT1.12 on
+#' microbead population 12} \item{MLC2v1}{a numeric vector, as MLC2v1
 #' of detection/capture probe MLC-2v/MLC-2v-cap on microbead population 1}
-#' \item{list("MLC2v2")}{a numeric vector, as MLC2v2 on microbead population 2}
-#' \item{list("MLC2v3")}{a numeric vector, as MLC2v3 on microbead population 3}
-#' \item{list("MLC2v4")}{a numeric vector, as MLC2v4 on microbead population 4}
-#' \item{list("MLC2v5")}{a numeric vector, as MLC2v5 on microbead population 5}
-#' \item{list("MLC2v6")}{a numeric vector, as MLC2v6 on microbead population 6}
-#' \item{list("MLC2v7")}{a numeric vector, as MLC2v7 on microbead population 7}
-#' \item{list("MLC2v8")}{a numeric vector, as MLC2v8 on microbead population 8}
-#' \item{list("MLC2v9")}{a numeric vector, as MLC2v9 on microbead population 9}
-#' \item{list("MLC2v10")}{a numeric vector, as MLC2v10 on microbead population
-#' 10} \item{list("MLC2v11")}{a numeric vector, as MLC2v11 on microbead
-#' population 11} \item{list("MLC2v12")}{a numeric vector, as MLC2v12 on
+#' \item{MLC2v2}{a numeric vector, as MLC2v2 on microbead population 2}
+#' \item{MLC2v3}{a numeric vector, as MLC2v3 on microbead population 3}
+#' \item{MLC2v4}{a numeric vector, as MLC2v4 on microbead population 4}
+#' \item{MLC2v5}{a numeric vector, as MLC2v5 on microbead population 5}
+#' \item{MLC2v6}{a numeric vector, as MLC2v6 on microbead population 6}
+#' \item{MLC2v7}{a numeric vector, as MLC2v7 on microbead population 7}
+#' \item{MLC2v8}{a numeric vector, as MLC2v8 on microbead population 8}
+#' \item{MLC2v9}{a numeric vector, as MLC2v9 on microbead population 9}
+#' \item{MLC2v10}{a numeric vector, as MLC2v10 on microbead population
+#' 10} \item{MLC2v11}{a numeric vector, as MLC2v11 on microbead
+#' population 11} \item{MLC2v12}{a numeric vector, as MLC2v12 on
 #' microbead population 12} }
 #' @seealso \code{\link{MFIerror}}, \code{\link{mcaSmoother}},
 #' \code{\link{diffQ}}, \code{\link{diffQ2}}, \code{\link{DMP}},
